0bbaeb74989d1de0a940bbdd08a1b4ccd30d522d
edgarw11 <edgarw11@gmail.com>
2015-09-19 14:50:56 -0300

    singleton

--- src/main/java/br/edu/inatel/task/api/TaskService.java
+++ src/main/java/br/edu/inatel/task/api/TaskService.java
@@ -3,9 +3,7 @@ package br.edu.inatel.task.api;
 import br.edu.inatel.task.model.Task;
 import br.edu.inatel.task.model.TaskDAO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,15 +15,46 @@ import java.util.ArrayList;
 @Path("/tasks")
 public class TaskService {
 
-    TaskDAO taskDAO = new TaskDAO();
+//    TaskDAO taskDAO = new TaskDAO();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response list() {
-        ArrayList<Task> tasks = taskDAO.list();
+        ArrayList<Task> tasks = TaskDAO.getInstance().list();
         return Response
                 .ok()
                 .entity(tasks)
+                .build();
+    }
+
+    @GET
+    @Path("{taskId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTaskById(@PathParam("taskId") Long id) {
+        Task task = TaskDAO.getInstance().getById(id);
+        return Response
+                .ok()
+                .entity(task)
+                .build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Task task){
+        Task created = TaskDAO.getInstance().create(task);
+        return Response
+                .status(201)
+                .entity(created)
+                .build();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(Task task){
+        Task created = TaskDAO.getInstance().update(task);
+        return Response
+                .ok()
+                .entity(created)
                 .build();
     }
 }
--- src/main/java/br/edu/inatel/task/model/TaskDAO.java
+++ src/main/java/br/edu/inatel/task/model/TaskDAO.java
@@ -1,5 +1,7 @@
 package br.edu.inatel.task.model;
 
+import jdk.nashorn.internal.objects.NativeArray;
+
 import java.util.ArrayList;
 
 /**
@@ -8,22 +10,58 @@ import java.util.ArrayList;
 public class TaskDAO {
 
     private ArrayList<Task> tasks;
+    private Long id = 0L;
+    private static TaskDAO taskDAO = null;
+
+    public static TaskDAO getInstance(){
+        if (taskDAO == null)
+            taskDAO = new TaskDAO();
+        return taskDAO;
+    }
 
     public TaskDAO() {
+
         tasks = new ArrayList<Task>();
-        tasks.add(new Task("Learn REST API"));
-        tasks.add(new Task("Learn Gradle"));
-        tasks.add(new Task("Learn Groovy"));
-        tasks.add(new Task("Learn Grails"));
-        tasks.add(new Task("Learn AngularJS"));
+        tasks.add(new Task("Learn REST API", id));
+        id++;
+        tasks.add(new Task("Learn Gradle", id));
+        id++;
+        tasks.add(new Task("Learn Groovy", id));
+        id++;
+        tasks.add(new Task("Learn Grails", id));
+        id++;
+        tasks.add(new Task("Learn AngularJS", id));
+        id++;
     }
 
     public ArrayList<Task> list(){
         return tasks;
     }
 
-    public void create(Task task){
-        task.setId(22L);
+    public Task getById(Long id){
+
+        Task task = null;
+
+        for (Task t : tasks){
+            if (t.getId() == id)
+                task = t;
+        }
+
+        return task;
+
+    }
+
+    public Task create(Task task){
+        task.setId(id);
         tasks.add(task);
+        id++;
+        return task;
+    }
+
+    public Task update(Task task){
+        int ind = tasks.indexOf(getById(task.getId()));
+        tasks.remove(ind);
+        tasks.add(ind, task);
+        return task;
     }
 }
