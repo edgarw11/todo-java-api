a2c9e14561e081d68d97f762736b87aab3b513fa
edgarw11 <edgarw11@gmail.com>
2015-09-19 11:50:05 -0300

    first commit

---
+++ src/main/java/br/edu/inatel/task/model/TaskDAO.java
@@ -0,0 +1,29 @@
+package br.edu.inatel.task.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Edgar on 19/09/2015.
+ */
+public class TaskDAO {
+
+    private ArrayList<Task> tasks;
+
+    public TaskDAO() {
+        tasks = new ArrayList<Task>();
+        tasks.add(new Task("Learn REST API"));
+        tasks.add(new Task("Learn Gradle"));
+        tasks.add(new Task("Learn Groovy"));
+        tasks.add(new Task("Learn Grails"));
+        tasks.add(new Task("Learn AngularJS"));
+    }
+
+    public ArrayList<Task> list(){
+        return tasks;
+    }
+
+    public void create(Task task){
+        task.setId(22L);
+        tasks.add(task);
+    }
+}
